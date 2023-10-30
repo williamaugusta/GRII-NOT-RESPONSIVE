56dc328be5313114b2f5bfb6e06455f4154874bd
william augusta <99062128+williamaugusta@users.noreply.github.com>
2023-10-30 21:56:27 +0700

    w

---
+++ GRIISOLO/GRII_SOLO/GRIISOLO/Controllers/GRII_SOLOController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GRIISOLO.Models;
+
+namespace GRIISOLO.Controllers
+{
+    public class GRII_SOLOController : Controller
+    {
+        // GET: GRII_SOLO
+        public ActionResult Index()
+        {
+            string[] filePaths = Directory.GetFiles(Server.MapPath("~/images_content/"));
+            List<Slider> files = new List<Slider>();
+            foreach (string filePath in filePaths)
+            {
+                string fileName = Path.GetFileName(filePath);
+                files.Add(new Slider
+                {
+                    title = fileName.Split('.')[0].ToString(),
+                    src = "/images_content/" + fileName
+                });
+            }
+            return View(files);
+        }
+
+        public ActionResult Visi_Misi()
+        {
+            return View();
+        }
+
+        public ActionResult Pengakuan_Iman()
+        {
+            return View();
+        }
+        public ActionResult Beban_Doa()
+        {
+            return View();
+        }
+        public ActionResult Profil_Hamba_Tuhan()
+        {
+            return View();
+        }
+        public ActionResult Gerakan_Grii()
+        {
+            return View();
+        }
+        public ActionResult Kegiatan()
+        {
+            return View();
+        }
+    }
+}
---
+++ GRIISOLO/GRII_SOLO/GRIISOLO/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GRIISOLO.MvcApplication" Language="C#" %>
